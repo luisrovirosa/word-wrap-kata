64468701f1b2f7cf833a88ef37a179db49b241bb
Luis Rovirosa <luisrovirosa@gmail.com>
2017-06-12 19:19:43 +0200

    one_word_bigger_than_number_of_columns_is_wrapped_by_the_number_of_columns

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -1,5 +1,8 @@
 public class WordWrap {
-    public String wrap(String text, int i) {
-        return text;
+    public String wrap(String text, int numberOfColumns) {
+        if (text.length() <= numberOfColumns){
+            return text;
+        }
+        return text.substring(0, numberOfColumns) + "\n" + text.substring(numberOfColumns);
     }
 }
--- src/test/java/WordWrapKataTest.java
+++ src/test/java/WordWrapKataTest.java
@@ -12,4 +12,13 @@ public class WordWrapKataTest {
 
         assertThat(text, is("hola a todos"));
     }
+
+    @Test
+    public void one_word_bigger_than_number_of_columns_is_wrapped_by_the_number_of_columns(){
+        WordWrap wrapper = new WordWrap();
+
+        String text = wrapper.wrap("hola", 2);
+
+        assertThat(text, is("ho\nla"));
+    }
 }
