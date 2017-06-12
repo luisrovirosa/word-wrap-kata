bbe3e923f127580cb0781f020fc0ab11418b9daa
Luis Rovirosa <luisrovirosa@gmail.com>
2017-06-12 19:36:21 +0200

    refactor: test extract method

--- src/test/java/WordWrapKataTest.java
+++ src/test/java/WordWrapKataTest.java
@@ -6,19 +6,16 @@ import static org.junit.Assert.assertThat;
 public class WordWrapKataTest {
     @Test
     public void does_not_wrap_when_line_is_smaller_than_the_number_of_columns(){
-        WordWrap wrapper = new WordWrap();
-
-        String text = wrapper.wrap("hola a todos", 20);
-
-        assertThat(text, is("hola a todos"));
+        assertThat(wrap("hola a todos", 20), is("hola a todos"));
     }
 
     @Test
     public void one_word_bigger_than_number_of_columns_is_wrapped_by_the_number_of_columns(){
+        assertThat(wrap("hola", 2), is("ho\nla"));
+    }
+
+    private String wrap(String text, int numberOfColumns) {
         WordWrap wrapper = new WordWrap();
-
-        String text = wrapper.wrap("hola", 2);
-
-        assertThat(text, is("ho\nla"));
+        return wrapper.wrap(text, numberOfColumns);
     }
 }
