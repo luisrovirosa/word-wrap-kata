97374c26aea33fe3f4a6aea31da55009ca0aaa5c
Luis Rovirosa <luisrovirosa@gmail.com>
2017-06-13 08:27:23 +0200

    refactor: extract method secondWrap

--- src/main/java/WordWrap.java
+++ src/main/java/WordWrap.java
@@ -3,16 +3,19 @@ public class WordWrap {
         if (text.length() <= numberOfColumns){
             return text;
         }
-        int beginningOfSecondWrap = hasSpacesWithinColumns(text, numberOfColumns) ? positionOfSpace(text) + 1 : numberOfColumns;
-        String secondWrap = wrap(text.substring(beginningOfSecondWrap), numberOfColumns);
         return firstWrap(text, numberOfColumns)
                 + "\n"
-                + secondWrap;
+                + secondWrap(text, numberOfColumns);
     }
 
     private String firstWrap(String text, int numberOfColumns) {
         int endOfFirstWrap = hasSpacesWithinColumns(text, numberOfColumns) ? positionOfSpace(text) : numberOfColumns;
         return text.substring(0, endOfFirstWrap);
+    }
+
+    private String secondWrap(String text, int numberOfColumns) {
+        int beginningOfSecondWrap = hasSpacesWithinColumns(text, numberOfColumns) ? positionOfSpace(text) + 1 : numberOfColumns;
+        return wrap(text.substring(beginningOfSecondWrap), numberOfColumns);
     }
 
     private boolean hasSpacesWithinColumns(String text, int numberOfColumns) {
